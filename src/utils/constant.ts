374d3a80b22a961bf915b4e46c3d4c8c6e5beb50
sunisaIoK <sunisaixk@gmail.com>
2025-01-27 23:11:58 +0700

    feat : การเชื่อมต่อฐานข้อมูล และดึงข้อมูลมาเก็บที่ constant และ Nextauth

---
+++ src/utils/constant.ts
@@ -0,0 +1,12 @@
+const DB_COLLECTIONS = {
+    DELIVERY_SERVICE: 'DeliveryService',
+    BOOKING: 'Booking',
+    DETAILS_SERVICE: 'DetailService',
+    SERVICE: 'Service',
+    LOGIN: 'users',
+    ORDER: 'Orders',
+    HIS: 'his',
+    PAYMENT: 'StatusPayment'
+
+}
+export default DB_COLLECTIONS
